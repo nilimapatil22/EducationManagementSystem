e1e93e834e0104c8dfaa748f95b3595aee90679e
MadhuriShelar <78599230+MadhuriShelar@users.noreply.github.com>
2021-02-06 18:45:46 +0530

    ed

--- src/main/java/com/cg/boot/repository/CourseRepository.java
+++ src/main/java/com/cg/boot/repository/CourseRepository.java
@@ -1,7 +1,5 @@
 package com.cg.boot.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +8,6 @@ import com.cg.boot.model.Course;
 public interface CourseRepository extends JpaRepository<Course,Integer>{
     
 	Course findByCourseName(String courseName);
-
-	public List<Course> findAllByStudentId(int studentId);
 	
 
 }
