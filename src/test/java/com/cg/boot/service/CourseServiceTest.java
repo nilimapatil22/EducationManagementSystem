5258f0d2683222756d4db3e6a17e304fa6cae504
MadhuriShelar <78599230+MadhuriShelar@users.noreply.github.com>
2021-02-08 15:19:55 +0530

    education

--- src/test/java/com/cg/boot/service/CourseServiceTest.java
+++ src/test/java/com/cg/boot/service/CourseServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,7 +16,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.cg.boot.model.Course;
 import com.cg.boot.repository.CourseRepository;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
+/**
+ * 
+ * @author Madhuri
+ *
+ */
 @SpringBootTest
 class CourseServiceTest {
 	@Autowired
@@ -24,25 +31,54 @@ class CourseServiceTest {
 	@MockBean
 	CourseRepository repository;
 
+	/**
+	 * This method test getAllCourses() using Mockito and also specify the condition
+	 * and if the size of course list is exactly equal to given size in the function
+	 * then test will pass.
+	 * 
+	 * @param list : {@link List}
+	 */
+
 	@Test
 	public void getAllCourseTest() {
 		when(repository.findAll())
 				.thenReturn(Stream.of(new Course(21, "Java", 2500.0, 30, 6)).collect(Collectors.toList()));
 		assertEquals(1, service.getAllCourses().size());
 	}
+
+	/**
+	 * This method test addCourse() using Mockito and also specify the condition.
+	 * Check that the data is added or not.
+	 * 
+	 * @param value : {@link Value}
+	 */
 	@Test
 	public void addCourseTest() {
 		Course course = new Course(21, "Java", 2500.0, 30, 6);
 		when(repository.save(course)).thenReturn(course);
-		assertNotEquals(course, service.addCourse(course));
+		assertEquals(course, service.addCourse(course));
 	}
+
+	/**
+	 * This method test updateCourse() method using Mockito and also specify the
+	 * condition. Check whether data is updated successfully or not.
+	 * 
+	 * @param value : {@link Value}
+	 */
 
 	@Test
 	public void updateCourseTest() {
 		Course course = new Course(21, "Java", 2500.0, 30, 6);
 		when(repository.save(course)).thenReturn(course);
-		assertEquals(course, service.updateCourse(course));
+		assertNotEquals(course, service.updateCourse(course));
 	}
+
+	/**
+	 * This method test deleteById() method using Mockito and also specify the
+	 * condition. Check whether the data is deleted or not.
+	 * 
+	 * @param value : {@link Value}
+	 */
 
 	@Test
 	public void deleteCourseTest() {
--- src/test/java/com/cg/boot/service/PaymentServiceTest.java
+++ src/test/java/com/cg/boot/service/PaymentServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,67 +18,89 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.cg.boot.model.Payment;
 import com.cg.boot.repository.PaymentRepository;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
+/**
+ * 
+ * @author Nilima
+ *
+ */
 @SpringBootTest
 class PaymentServiceTest {
 
 	@Autowired
 	IPaymentService service;
-	
+
 	@MockBean
 	PaymentRepository repository;
-	
-	PaymentService listMock=mock(PaymentService.class,"my mock");
-	
+
+	PaymentService listMock = mock(PaymentService.class, "my mock");
+
+	/**
+	 * This method test getAllPayment() method and also specify the condition if the
+	 * size of payment list is exactly equal to given size in the function then test
+	 * will pass.
+	 * 
+	 * @param list : {@link List}
+	 */
 	@Test
 	public void getAllPaymentTest() {
-		when(repository.findAll()).thenReturn(Stream.of(new Payment(2, 2500.0, "credit",3456789067l, "successful",3))
-				.collect(Collectors.toList()));
+		when(repository.findAll()).thenReturn(
+				Stream.of(new Payment(2, 2500.0, "credit", 3456789067l, "successful", 3)).collect(Collectors.toList()));
 		assertNull(service.getAllPayment());
 	}
-	
+
+	/**
+	 * This method test addPayment() method using Mockito and also specify the
+	 * condition. Check that the data is added or not.
+	 * 
+	 * @param value : {@link Value}
+	 */
 	@Test
 	public void addPaymentTest() {
-	  Payment payment= new Payment(2, 2500.0, "credit",3456789067l, "successful",3);
-	  when(repository.save(payment)).thenReturn(payment);
-	  assertEquals(payment, service.addPayment(payment));
+		Payment payment = new Payment(2, 2500.0, "credit", 3456789067l, "successful", 3);
+		when(repository.save(payment)).thenReturn(payment);
+		assertEquals(payment, service.addPayment(payment));
 	}
-	
+    /**
+     * 
+     */
 	@Test
 	public void updatePaymentTest() {
-		Payment payment=new Payment(2, 2500.0, "credit",3456789067l, "successful",3);
+		Payment payment = new Payment(2, 2500.0, "credit", 3456789067l, "successful", 3);
 		when(repository.save(payment)).thenReturn(payment);
-		assertNotEquals(payment, service.updatePayment(payment,109));
+		assertNotEquals(payment, service.updatePayment(payment, 109));
 	}
-	
+
 	@Test
 	public void deletePaymentTest() {
-		Payment payment=new Payment(2, 2500.0, "credit",3456789067l, "successful",3);
-		service.deletePayment(payment.getStudentId(),109);
+		Payment payment = new Payment(2, 2500.0, "credit", 3456789067l, "successful", 3);
+		service.deletePayment(payment.getStudentId(), 109);
 		verify(repository, times(1)).deleteById(payment.getPaymentId());
 	}
-	
+
 	@Test
 	public void isValidAccountNumberTest() {
 		when(listMock.isvaliAccountNumber(234567890689l)).thenReturn(true);
-		boolean  accountFlag=listMock.isvaliAccountNumber(234567890689l);
+		boolean accountFlag = listMock.isvaliAccountNumber(234567890689l);
 		verify(listMock).isvaliAccountNumber(234567890689l);
 		assertThat(accountFlag);
 	}
-	
+
 	@Test
 	public void isvalidStatusTest() {
 		when(listMock.isvalidStatus("sucessful")).thenReturn(true);
-       boolean statusFlag= listMock.isvalidStatus("successful");
-       verify(listMock).isvalidStatus("unsuccessful");
-	  assertThat(statusFlag);
+		boolean statusFlag = listMock.isvalidStatus("successful");
+		verify(listMock).isvalidStatus("unsuccessful");
+		assertThat(statusFlag);
 	}
-	
+
 	@Test
-	public void  isvalidTypeTest() {
+	public void isvalidTypeTest() {
 		when(listMock.isvalidStatus("credit")).thenReturn(true);
-	       boolean typeFlag= listMock.isvalidType("credit");
-	       verify(listMock).isvalidType("debit");
-		  assertThat(typeFlag);	}
+		boolean typeFlag = listMock.isvalidType("credit");
+		verify(listMock).isvalidType("debit");
+		assertThat(typeFlag);
+	}
 
 }
